19d44e16fb8a279931fab196204833dbbdaf0e22
xArtXtrAx <132019928+xArtXtrAx@users.noreply.github.com>
2026-08-02 03:18:00 -0600

    Aislar reactor durante Cámara Cromática

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './ChromaticChamberGuard.css'
 import App from './App.tsx'
 import { ChromaticChamberSystem } from './ChromaticChamberSystem'
 import { ChromaticGamepadBridge } from './ChromaticGamepadBridge'
