965ebe54470957ccda9c2de2a1c2614e8bcb7882
xArtXtrAx <132019928+xArtXtrAx@users.noreply.github.com>
2026-08-02 02:48:41 -0600

    Asegurar prioridad visual del Gatillo

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './PulseTriggerLevel.css'
 import App from './App.tsx'
 import { GamepadController } from './GamepadController'
 import { GamepadEventHaptics } from './GamepadEventHaptics'
 import { PulseTriggerSystem } from './PulseTriggerSystem'
 import { RegionFocusGuide } from './RegionFocusGuide'
+import './PulseTriggerLevel.css'
 
 const editableSelector =
   'input, textarea, [contenteditable="true"], [data-allow-selection="true"]'
